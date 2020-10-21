a5b7eeecf319733c75f79102e1792d67f0d1ccaa
Mihail Yarantsev <mihaalich2@mail.ru>
2020-10-21 16:28:18 +0300

    create structure project and add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom"
+import {Header} from "./common/Header";
+import {Footer} from "./common/Footer";
+import {Home} from "./components/Home";
+import {Category} from "./components/Category";
+import {Product} from "./components/Product";
+import {Cart} from "./components/Cart";
+import {NotFound} from "./components/NotFound";
 
 function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/category/:categoryId" component={Category}/>
+                <Route path="/product/:productId" component={Product}/>
+                <Route path="/cart" component={Cart}/>
+                <Route path="/" component={NotFound} />
+            </Switch>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
