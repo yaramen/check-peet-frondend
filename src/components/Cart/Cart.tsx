821d61a07416e0c5ade693dc8579d304f54f01ba
Mihail Yarantsev <mihaalich2@mail.ru>
2020-10-22 12:24:25 +0300

    add cart calculations

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import {useCartContext} from "./CartContext";
 
 const Table = styled.table`
   width: 100%;
@@ -10,53 +11,58 @@ const Td = styled.td`
   text-align: center;
 `;
 
+const Mark = styled.span`
+  color: darkred;
+  font-weight: bold;
+`;
+
 export const Cart = () => {
+    const {products, updateAmount, getSums, remove} = useCartContext();
+    const {
+        calories,
+        protein,
+        fat,
+        carbohydrate
+    } = getSums();
+
     return (
         <Table>
             <thead>
-                <tr>
-                    <th>Продукт</th>
-                    <th>Белки</th>
-                    <th>Жиры</th>
-                    <th>Улгеводы</th>
-                    <th>Калории</th>
-                    <th>Кол-во гр</th>
-                </tr>
+            <tr>
+                <th>Продукт</th>
+                <th>Белки, в 100 гр</th>
+                <th>Жиры, в 100 гр</th>
+                <th>Углеводы, в 100 гр</th>
+                <th>Калории, в 100 гр</th>
+                <th>Кол-во гр</th>
+                <th></th>
+            </tr>
             </thead>
             <tbody>
-                <tr>
-                    <Td>Американская смесь Hortex</Td>
-                    <Td>2.4</Td>
-                    <Td>0.5</Td>
-                    <Td>5.1</Td>
-                    <Td>39</Td>
-                    <Td><input type="number" defaultValue={100} /></Td>
+            {products.map(product => (
+                <tr key={product.id}>
+                    <Td>{product.name}</Td>
+                    <Td>{product.protein}</Td>
+                    <Td>{product.fat}</Td>
+                    <Td>{product.carbohydrate}</Td>
+                    <Td>{product.calories}</Td>
+                    <Td><input type="number" value={product.amount}
+                               onChange={(e) => updateAmount(product, +e.target.value)}/></Td>
+                    <Td>
+                        <button onClick={() => remove(product.id)}>удалить</button>
+                    </Td>
                 </tr>
-                <tr>
-                    <Td>Американская смесь Hortex</Td>
-                    <Td>2.4</Td>
-                    <Td>0.5</Td>
-                    <Td>5.1</Td>
-                    <Td>39</Td>
-                    <Td><input type="number" defaultValue={100} /></Td>
-                </tr>
-                <tr>
-                    <Td>Американская смесь Hortex</Td>
-                    <Td>2.4</Td>
-                    <Td>0.5</Td>
-                    <Td>5.1</Td>
-                    <Td>39</Td>
-                    <Td><input type="number" defaultValue={100} /></Td>
-                </tr>
+            ))}
             </tbody>
             <tfoot>
-                <tr>
-                    <Td>Итого</Td>
-                    <Td>25</Td>
-                    <Td>13</Td>
-                    <Td>14</Td>
-                    <Td><b>13</b></Td>
-                </tr>
+            <tr>
+                <Td>Итого</Td>
+                <Td>{protein}</Td>
+                <Td>{fat}</Td>
+                <Td>{carbohydrate}</Td>
+                <Td><Mark>{calories}</Mark></Td>
+                <Td></Td>
+            </tr>
             </tfoot>
         </Table>
     )
--- src/components/Cart/useCart.ts
+++ src/components/Cart/useCart.ts
@@ -10,19 +10,50 @@ export const useCart = () => {
     };
 
     const add = (product: Product) => {
-        if(!isSet(product)) {
+        if (!isSet(product)) {
             setProducts([...products, {...product, amount: 100}]);
         }
     }
 
-    const remove = (productId: number|string) => {
+    const remove = (productId: number | string) => {
         setProducts(products.filter(product => product.id !== productId))
     }
+
+    const updateAmount = (product: Product, amount: number) => {
+        setProducts(products.map(currProduct => {
+            if (currProduct.id === product.id) {
+                return {
+                    ...product,
+                    amount
+                }
+            }
+            return currProduct
+        }));
+    }
+
+    const getSums = () => {
+        return products.reduce((ac, curr) => {
+            if (curr.amount) {
+                ac.calories = Math.round(ac.calories + curr.calories * (curr?.amount / 100));
+                ac.protein = Math.round(ac.protein + curr.protein * (curr?.amount / 100));
+                ac.fat = Math.round(ac.fat + curr.fat * (curr?.amount / 100));
+                ac.carbohydrate += Math.round(ac.carbohydrate + curr.carbohydrate * (curr?.amount / 100));
+            }
+            return ac;
+        }, {
+            calories: 0,
+            protein: 0,
+            fat: 0,
+            carbohydrate: 0
+        })
+    };
 
     return {
         products,
         isSet,
         add,
         remove,
+        updateAmount,
+        getSums
     };
 };
